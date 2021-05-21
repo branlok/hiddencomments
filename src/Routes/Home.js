cb47329a59dca0a4ec242e735378bb042efa5132
Brandon Lok <brandonlok@Brandons-MacBook-Pro.local>
2021-05-21 15:14:05 -0400

    test

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,9 +1,7 @@
 import React from "react";
 
 function Home() {
-  return <div className="bg-red-500 h-full w-full">
-      testn
-  </div>;
+  return <div className="bg-red-500 h-full w-full border border-black">testn</div>;
 }
 
 export default Home;
