0f085b16749f4f61c5efa5d9d1c554ee896e8e0c
Brandon Lok <brandonlok@Brandons-MacBook-Pro.local>
2021-05-21 15:16:40 -0400

    testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 // tailwind.config.js
 module.exports = {
-  purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
